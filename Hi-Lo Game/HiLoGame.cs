70197076bddee97155330dd93c33c749df9a3ebe
syuan7492 <kurosaki7492@gmail.com>
2022-04-24 19:12:25 +0800

    Add project files.

---
+++ Hi-Lo Game/HiLoGame.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hi_Lo_Game
+{
+     static class HiLoGame
+    {
+        public const int MAXIMUM = 10;
+        private static Random random = new Random();
+        private static int currentNumber = random.Next(1, MAXIMUM + 1);
+        private static int pot = 10;
+
+        //Main方法可以取得GotPot回傳的值，但無法修改它
+        public static int GetPot() { return pot; }
+        public static int GetCurrentNumber() { return currentNumber; }
+
+        public static void Guess(bool higher)
+        {
+            int nextNumber = random.Next(1, MAXIMUM + 1);
+            
+            if ((higher && nextNumber >= currentNumber) || (!higher && nextNumber <= currentNumber))
+            {
+                Console.WriteLine("\n你猜對囉!!");
+                pot+=3;
+            }
+
+            else
+            {
+                Console.WriteLine("\n真不幸, 你猜錯囉!!");
+                pot-=3;
+            }
+
+            currentNumber = nextNumber;
+
+            Console.WriteLine($"目前的數字是 {currentNumber}");
+        }
+
+        /*public static void Hint()
+        {
+            int half = MAXIMUM / 2;
+            
+            //currentNumber的值還沒變成nextNumber
+            if (currentNumber >= half)
+            {
+                Console.WriteLine($"\n這個數字至少是 {half}");
+            }
+
+            else
+            {
+                Console.WriteLine($"\n這個數字最多是 {half}");
+            }
+
+            pot-=2;
+        }*/
+
+    }
+}
---
+++ Hi-Lo Game/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Hi_Lo_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //列出Random的陣列內容
+            /*
+            HiLoGame.random = new Random(1);
+            Random seededRandom = new Random(1);
+            Console.Write("前20個隨機數是: ");
+            for (int i = 0; i < 10; i++)
+                Console.Write($"{ seededRandom.Next(1, HiLoGame.MAXIMUM + 1)} ");*/
+            
+            Console.WriteLine($"歡迎參加猜數字遊戲,目前的數字是{HiLoGame.GetCurrentNumber()}");
+            Console.WriteLine($"數字區間在 1和 {HiLoGame.MAXIMUM} 之間");
+
+            while (HiLoGame.GetPot() > 0)
+            {
+                Console.WriteLine($"\n你有 {HiLoGame.GetPot()}元");
+
+                Console.WriteLine("\n下一個數字>=目前數字大請輸入 1, 下一個數字<=目前數字小請輸入 2, \n結束遊戲請輸入其他按鍵");
+
+                char key = Console.ReadKey(true).KeyChar;
+                if (key == '1')
+                {
+                    HiLoGame.Guess(true);
+                }
+
+                else if (key == '2')
+                {
+                    HiLoGame.Guess(false);
+                }
+
+                /*else if (key == '3')
+                {
+                    HiLoGame.Hint();
+                }*/
+
+                else return;
+            }
+
+            Console.WriteLine("\n你口袋空空囉, Bye~~");
+        }
+    }
+}
